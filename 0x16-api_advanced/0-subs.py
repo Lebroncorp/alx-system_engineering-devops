a96ff0fd45d33484d54249b9add20c39f4140830
Lebroncorp <lebroncorp@yahoo.com>
2024-02-13 15:55:55 -0800

    sub update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""
+queries the Reddit API and returns the number of subscribers
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """returns the number of subscribers of a given subreddit"""
+    if subreddit is None or type(subreddit) is not str:
+        return 0
+
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': '0x16-api_advanced:project:\
+v1.0.0 (by /u/lebroncorp_tech_jr)'}).json()
+
+    subs = r.get("data", {}).get("subscribers", 0)
+    return sub
