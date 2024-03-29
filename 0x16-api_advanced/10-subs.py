492b788eace9db2267e6d6c92a6f9fe200df8721
Lebroncorp <lebroncorp@yahoo.com>
2024-03-28 18:14:29 -0700

    asd

--- 0x16-api_advanced/10-subs.py
+++ 0x16-api_advanced/10-subs.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
 """
-Contains the number_of_subscribers function
+queries the Reddit API and returns the number of subscribers
 """
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers for a given subreddit"""
+    """returns the number of subscribers of a given subreddit"""
     if subreddit is None or type(subreddit) is not str:
         return 0
+
     r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
                      headers={'User-Agent': '0x16-api_advanced:project:\
-v1.0.0 (by /u/newman_tech_jr)'}).json()
+v1.0.0 (by /u/lebroncorp_tech_jr)'}).json()
+
     subs = r.get("data", {}).get("subscribers", 0)
-    return sub
+    return subs
