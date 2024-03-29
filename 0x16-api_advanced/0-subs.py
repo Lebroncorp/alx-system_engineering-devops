ae01ae515cdb5a606eb867817eab0274657e6044
Lebroncorp <lebroncorp@yahoo.com>
2024-03-28 18:31:01 -0700

    top ten

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,23 +1,26 @@
 #!/usr/bin/python3
 """
-    Queries the Reddit API and returns the number of subscribers
-    (not active users, total subscribers) for a given subreddit
+queries the Reddit API and returns the number of subscribers
 """
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers of a given subreddit"""
-    if subreddit is None or type(subreddit) is not str:
+    """
+    queries the Reddit API and returns the number of
+    subscribers (not active users, total subscribers)
+    for a given subreddit
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+        'AppleWebKit/537.36'
+        '(KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+        'Edg/91.0.864.59'
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
+    else:
         return 0
-
-    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
-                     headers={'User-Agent': '0x16-api_advanced:project:\
-v1.0.0 (by /u/nenyeikpa)'}, allow_redirects=False).json()
-
-    if r.statusCode == 302:
-        return 0
-
-    if r.statusCode == 200:
-        subs = r.get("data", {}).get("subscribers", 0)
-    return subs
